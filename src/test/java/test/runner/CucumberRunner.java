07431de1fc782456bcd9a2ad29bc4752179b1455
lasamegrelidze <lidzeGit@gmail.com>
2019-05-23 22:29:10 +0300

    ref

--- src/test/java/test/runner/CucumberRunner.java
+++ src/test/java/test/runner/CucumberRunner.java
@@ -1,12 +1,11 @@
-package test.feature;
+package test.runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/test/resources/"},
+@CucumberOptions(features = {"src/test/java/test/feature/"},
         glue = {"selenium/steps/"},
         plugin = {"pretty",
         "selenium.reporter.AllureReport"},
