f0b22f1a344929061778f6efed8652ca1c6fa725
lasamegrelidze <lidzeGit@gmail.com>
2019-05-24 10:32:29 +0300

    ref

--- src/main/java/selenium/pages/BasePageObject.java
+++ src/main/java/selenium/pages/BasePageObject.java
@@ -2,10 +2,8 @@ package selenium.pages;
 
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
